4eb9911380d6f50aa40c5167d63be7cb66047eaf
bishk <abhishekdpseok@gmail.com>
2016-10-15 12:58:46 -0400

    fixed so that works for multiple cities

--- router.js
+++ router.js
@@ -26,8 +26,9 @@ module.exports = function (app) {
     });
     
     app.post('/addSpot', function (req, res) {
-        var lat = req.body.lat;
-        var lng = req.body.lng;
+        var lat = req.body.lat2;
+        var lng = req.body.lng2;
+        console.log(lat + "," + lng);
         var post  = { Lat: lat, Lng : lng };
         con.query("INSERT INTO spots SET ?", post, function (err, rows) {
           if(err)
@@ -40,70 +41,99 @@ module.exports = function (app) {
     
     app.post('/findSpot', function (req, res) 
     { 
-          var lat = req.body.lat;
-          var lng = req.body.lng;
+          var lat = req.body.lat1;
+          var lng = req.body.lng1;
           var dataset = [];
           var dataset2 = [];
           var finalrray = [];
           var delta = {};
+          var idek = 0;
+
           con.query('SELECT Lat, Lng, Time, Id FROM spots', 
           function(err,rows) 
           {
-            if(err) throw err;
-            console.log('Data received from Db:\n');
-            console.log(rows.length);
             
-            var origin = lat + "," + lng;
-
-      //var destinations = ['Chicago, United States of America', 'Denver, United States of America', 'Vancouver, Canada', 'Michigan, United States of America'];
-            var seconds = ((new Date).getTime())/1000;
-      //var times = [(seconds-1475936281)/60, (seconds-1475936282)/60, (seconds-1475936281)/60, (seconds-1475936281)/60
-            var longlats =  new Array();
-            console.log(rows);
-            console.log(rows.length);
-            for(var i=0; i<rows.length; i++)
+            if(rows[0])
             {
-              var something = rows[i].Lat + "," + rows[i].Lng;
-              longlats.push(something);
-            }
-            var data;
-            var to = new Array();
-            for(var i=0; i<rows.length; i++)
-            { 
-              var something = (seconds-(rows[i].Time/1000))/60;
-              to.push(something);
-            }
-            for(var k = 0; k<rows.length; k++){
-              var distServ = require('google-distance');
-            var x = distServ.get(
-              {
-                origins : [origin],
-                destinations : [longlats[k]], 
-                mode : 'driving'
-              },
-            function(err, data)
-              { 
-                if(err)
-                  console.log("Error");
-              dataset.push([data[0].destination, data[0].durationValue/60]);
-              if(dataset.length==rows.length){
-              for(var bo = 0; bo<rows.length; bo++){
-                dataset2.push([dataset[bo][0].substring(0,dataset[bo][0].length-15), Math.ceil(dataset[bo][1]), Math.ceil(to[bo]), 3*to[bo]+dataset[bo][1], rows[bo].Id]);
-              }
-               function sortFunction(a, b) {
-              if (a[3] === b[3]) {
-               return 0;
+              var origin = lat + "," + lng;
+              
+          
+      //var destinations = ['Chicago, United States of America', 'Denver, United States of America', 'Vancouver, Canada', 'Michigan, United States of America'];
+              var seconds = ((new Date).getTime())/1000;
+              console.log(seconds);
+      //var times = [(seconds-1475936281)/60, (seconds-1475936282)/60, (seconds-1475936281)/60, (seconds-1475936281)/60
+              var longlats =  new Array();
+              for(var i=0; i<rows.length; i++)
+                {
+                var something = rows[i].Lat + "," + rows[i].Lng;
+                longlats.push(something);
                 }
-                else {
-                 return (a[3] < b[3]) ? -1 : 1;
-                  }}
-              var sorted_dataset = dataset2.sort(sortFunction);            
-              //res.send(sorted_dataset);
-              res.render('feed', {dataset: sorted_dataset, lat : lat, lng : lng});
-              //res.render('index', { msg1: '' + dataset2, msg2: ''+to});
+                
+              var to = new Array();
+              for(var i=0; i<rows.length; i++)
+              { 
+                var something = (seconds-(rows[i].Time/1000))/60;
+                to.push(something);
               }
-              }); 
-            }
+            
+              for(var k = 0; k<rows.length; k++)
+              {
+                
+                var distServ = require('google-distance');
+                var x = distServ.get(
+                    {
+                      origins : [origin],
+                      destinations : [longlats[k]], 
+                      mode : 'driving'
+                    },
+                  function(err, data)
+                    { 
+                      idek++;
+                      if(data)  
+                        {
+                          
+                          var index = dataset.push([data[0].destination, data[0].durationValue/60]);
+                          
+                          console.log(index);
+                          if((dataset[index-1][1])<30)
+                            dataset2.push([dataset[idek-1][0].substring(0,dataset[idek-1][0].length-15), Math.ceil(dataset[idek-1][1]), Math.ceil(to[idek-1]), 3*to[idek-1]+dataset[idek-1][1], rows[idek-1].Id]);
+                          
+                    
+                          function sortFunction(a, b) 
+                            {
+                              if (a[3] === b[3])
+                                return 0;
+                              else
+                                return (a[3] < b[3]) ? -1 : 1;
+                            }
+                            
+          
+              
+                          if(idek==rows.length)
+                            {
+                            dataset2 = dataset2.sort(sortFunction); 
+                            res.render('feed', {dataset : dataset2, lat : lat, lng : lng});
+                            }
+                          
+            
+                        }
+                    });
+                }
+                
+
+            }  
+            
+            else
+              {
+                
+                var errm = "No spots nearby...";
+                res.render('feed', {lat : lat, lng : lng, errm : errm});
+              }
+            
+
+              //res.render('index', { msg1: '' + dataset2, msg2: ''+to});
+         }); 
+      });
                /*  function sortFunction(a, b)
               {
                 if (a[2] === b[2]) 
@@ -113,18 +143,16 @@ module.exports = function (app) {
               }
               var sorted_dataset = dataset.sort(sortFunction);
                */
-            });
-     });
      
      
       app.get('/spot/:id/:lat/:lng/:itemzero/', function(req, res) {
      
-     rLat = req.param('lat');
-     rLng = req.param('lng');
-     rDest = req.param('itemzero');
-     rName = rDest;
-     rDest = rDest.replace(/ /g, "+");
-     rID = req.param('id');
+     var rLat = req.param('lat');
+     var rLng = req.param('lng');
+     var rDest = req.param('itemzero');
+     var rName = rDest;
+     var rDest = rDest.replace(/ /g, "+");
+     var rID = req.param('id');
      
      
        var originp = rLat + "," + rLng;
